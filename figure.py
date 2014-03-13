a4e46e0dae77c4c604f79307043a4cfb89eb12ce
Anton Guryanov <guryanov91@gmail.com>
2014-03-13 17:53:58 +0400

    Stubs for figure classes.

---
+++ figure.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+
+class Figure:
+    @staticmethod
+    def is_valid_move(from_cell, to_cell):
+        pass
+
+    @staticmethod
+    def intermediate_cells(from_cell, to_cell):
+        pass
+
+
+class Pawn(Figure):
+    pass
+
+
+class Rook(Figure):
+    pass
+
+
+class Knight(Figure):
+    pass
+
+
+class Bishop(Figure):
+    pass
+
+
+class Queen(Figure):
+    pass
+
+
+class King(Figure):
+    pass
+
